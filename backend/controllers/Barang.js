60bbbb686e1ce87d1fbfcc0eaad69c0638ac0a10
RendyCahyaE <rendycahya6@gmail.com>
2022-06-16 23:18:05 +0700

    Done BE Bagian Barang

--- backend/controllers/Barang.js
+++ backend/controllers/Barang.js
@@ -2,6 +2,7 @@ import db from "../config/Database.js";
 import barang from "../models/Barang.js";
 import category_barang from "../models/CategoryBarang.js";
 import path from "path";
+import fs from "fs";
 
 export const getAllBarang = async (req,res ) =>{
     try {
@@ -14,18 +15,17 @@ export const getAllBarang = async (req,res ) =>{
 
 export const getOneBarang = async (req, res) =>{   
     try {
-        const [results, metadata] = await db.query("SELECT * FROM barang INNER JOIN category_barang ON category_barang.id_category = barang.id_category_barang WHERE barang.id ="+ req.params.id);
-        // const getOne = await barang.findOne({
-        //     where: {
-        //         id: req.params.id,
-        //     },
-        //     include: {
-        //         model: category_barang,
-        //         as: category_barang,
-        //     }
-
-        // });
-        res.status(200).json({results});
+        const getOne = await barang.findOne({
+            where: {
+                id: req.params.id,
+            }
+        });
+        if(getOne){
+            const [results, metadata] = await db.query("SELECT * FROM barang INNER JOIN category_barang ON category_barang.id_category = barang.id_category_barang WHERE barang.id ="+ req.params.id);
+            res.status(200).json({results});
+        }else{
+            res.status(404).json({msg: "Data Tidak Ada!"})
+        }
     } catch (error) {
         res.status(404).json(error)
     }
@@ -33,12 +33,12 @@ export const getOneBarang = async (req, res) =>{
 
 export const saveBarang = async (req,res) => {
     if(req.files === null) return res.status(400).json({msg: "No File Uploaded"});
-    const name = req.body.title;
+    const name = req.body.nama_barang;
     const file = req.files.file;
     const fileSize = file.data.lenght;
     const ext = path.extname(file.name);
     const fileName = file.md5 + ext;
-    const url = `${req.protocol}://${req.get("host")}/images/${name}`;
+    const url = `${req.protocol}://localhost:5000/images/${fileName}`;
     const allowedType = ['.png','.jpg', '.jpeg'];
 
     if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
@@ -49,21 +49,92 @@ export const saveBarang = async (req,res) => {
         if(err) return res.status(500).json({msg: err.message});
         try {
             await barang.create({
-                
-            })
+                nama_barang: name,
+                harga_barang: req.body.harga_barang,
+                id_category_barang: req.body.id_category_barang,
+                stok_barang: req.body.stok_barang,
+                deskripsi_barang : req.body.deskripsi_barang,
+                foto_barang: fileName,
+                url: url,
+            });
+            res.status(201).json({msg: "Product Telah Dibuat.", url: url});
         } catch (error) {
-            
+            console.log(error.message);
         }
     })
 
 }
 
 export const updateBarang = async (req,res) => {
+    const getOne = await barang.findOne({
+        where: {
+            id: req.params.id,
+        }
+    });
+    if(!getOne) res.status(404).json({msg: "No Data Found!"});
+
+    let fileName = "";
+    if(req.files === null){
+        fileName = getOne.foto_barang;
+    }else{
+        const file = req.files.file;
+        const fileSize = file.data.lenght;
+        const ext = path.extname(file.name);
+        fileName = file.md5 + ext;
+        const allowedType = ['.png','.jpg', '.jpeg'];
+
+        if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({msg: "Invalid Images"});
+        if(fileSize > 5000000) return res.status(422).json({msg: "Image must be less than 5MB"});
+        
+        const filepath = `./public/images/${getOne.foto_barang}`;
+        fs.unlinkSync(filepath);
+
+        file.mv(`./public/images/${fileName}`, (err)=>{
+            if(err) return res.status(500).json({msg: err.message});
+            
+        })
+    }
+    const name = req.body.nama_barang;
+    const url = `${req.protocol}://localhost:5000/images/${fileName}`;
+    try {
+        await barang.update({
+            nama_barang: name,
+            harga_barang: req.body.harga_barang,
+            id_category_barang: req.body.id_category_barang,
+            stok_barang: req.body.stok_barang,
+            deskripsi_barang : req.body.deskripsi_barang,
+            foto_barang: fileName,
+            url: url,
+        },{
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json({msg: "Product Telah Berhasil Di Update"})
+    } catch (error) {
+        console.log(error.message);
+    }
 
 }
 
 export const deleteBarang = async (req,res) => {
-
+    const getOne = await barang.findOne({
+        where: {
+            id: req.params.id,
+        }
+    });
+    if(getOne){
+        const filepath = `./public/images/${getOne.foto_barang}`;
+        fs.unlinkSync(filepath);
+        await barang.destroy({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json({msg: "Barang Berhasil Dihapus!"})
+    }else{
+        res.status(404).json({msg: "No Data Found!"})
+    }    
 }
 
 
--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,25 @@
 import express  from "express";
-import db from "./config/Database.js";
 import cors from 'cors';
 import Routes from "./routes/Routes.js";
 import fileUpload from "express-fileupload";
 
 const app = express();
+
 app.use(cors())
 app.use(express.json());
 app.use(fileUpload());
+app.use(express.static("public"));
 app.use(Routes);
 
 
 
-try {
-    await db.authenticate();
-    console.log('Database Connected ...')
-    //Seperti Migration, Jika tidak ada tabel maka akan dibuatkan table secara otomatis, Cukup jalankan sekali saja, kalau sudah harap di comment
-    await db.sync();
-} catch (error) {
-    console.error(error);
-}
-
-
-
+// try {
+//     await db.authenticate();
+//     console.log('Database Connected ...')
+//     //Seperti Migration, Jika tidak ada tabel maka akan dibuatkan table secara otomatis, Cukup jalankan sekali saja, kalau sudah harap di comment
+//     await db.sync();
+// } catch (error) {
+//     console.error(error);
+// }
 
 app.listen(5000, ()=>console.log('Server running at port 5000'))
