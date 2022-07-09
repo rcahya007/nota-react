b29984dcd5aa199461db5fff47a3917f665da102
YourNewFriend <89686508+YourNewFriendd@users.noreply.github.com>
2022-07-09 18:21:54 +0700

    .

--- frontend/src/component/ModalFormTambahNota/ModalFormTambahNota.jsx
+++ frontend/src/component/ModalFormTambahNota/ModalFormTambahNota.jsx
@@ -6,7 +6,6 @@ const ModalFormTambahNota = ({closeModal}) => {
 
     const [dataBarangSemua, setDataBarangSemua] = useState([]);
     const [hasilAmbilData, setHasilAmbilData] = useState([]);
-    const [selectItem, setSelectItemn] = useState('');
     const [dataDipilih, setDataDipilih] = useState([]);
     const [maxPesan, setMaxPesan] = useState(1)
     const [deskripsi, setDeskripsi] = useState('');
@@ -14,9 +13,20 @@ const ModalFormTambahNota = ({closeModal}) => {
     const [total, setTotal] = useState(0);
     const namaBarang = useRef();
     const hargaBarang = useRef();
-    const [hasilPerkalian, setHasilPerkalian] = useState(0);
 
-    // console.log(hargaBarang.value);
+    // console.log(dataDipilih);
+
+    const TambahBarang = () => {
+        setDataBarangSemua({
+            id: 29,
+            nama_barang: dataDipilih.nama_barang,
+            banyak_barang: banyak,
+            harga_barang: dataDipilih.hargaBarang,
+            total_harga: total,
+        })
+    }
+
+    console.log(dataBarangSemua);
 
     useEffect(()=>{
         perkalian();
@@ -25,14 +35,11 @@ const ModalFormTambahNota = ({closeModal}) => {
     const minus = () => {
         setBanyak(banyak - 1);
         perkalian();
-        // console.log(banyak)
     }
 
     const plus = () => {
         setBanyak(banyak + 1);
         perkalian();
-        // console.log(banyak)
-
     }
     const perkalian = () => {
         const hasil = hargaBarang.current.value * banyak;
@@ -41,9 +48,6 @@ const ModalFormTambahNota = ({closeModal}) => {
         console.log(hasil)
         setTotal(hasil);
     }
-
-
-    console.log(dataDipilih)
 
     const getId = async (id) => {
         const getDataById = await axios.post(`http://localhost:5000/barang/selectId/`+id);
@@ -62,8 +66,6 @@ const ModalFormTambahNota = ({closeModal}) => {
 
     const getBarang = async (e) => {
         const nama_barang = (e.target.value);
-        // setNamaBarang(namaBarang);
-        // console.log(nama_barang);
         if(nama_barang === ''){
             setHasilAmbilData([])
         }else{
@@ -77,15 +79,6 @@ const ModalFormTambahNota = ({closeModal}) => {
         }       
     }
 
-    const getBanyak = (e) => {
-        const banyakInput = (e.target.value);
-        const hitung = dataDipilih.stok_barang - banyakInput;
-        if(hitung > 0) {
-            
-        }
-
-    }
-
     return (
         <div className="bg-black bg-opacity-50 inset-0 overflow-y-scroll fixed ">
             <div className="bg-white w-2/3 rounded-xl mx-auto my-4 xl:w-1/2 ">
@@ -97,96 +90,80 @@ const ModalFormTambahNota = ({closeModal}) => {
                         </button>
                     </div>
                 </div>
-                <form className='pb-4'>
-                    <div className='mt-3 mx-4'>
-                        <div className='mx-5 mt-2 mb-4'>
-                            <label htmlFor="nama" className=''>Nama Barang / Jasa : </label>
-                            <input id='nama_barang' type="text" className='mt-2 block border-2 border-slate-400 w-full rounded p-2 placeholder-shown:italic' placeholder='Masukkan Nama Barang' onChange={getBarang} ref={namaBarang}/>
-                            {
-                                dataDipilih.length > 0 ? 
-                                <div>
-                                    <p className='text-sm mt-1' id='span_stock'>Stock barang sekarang : <b>{dataDipilih[0].stok_barang}</b></p>
-                                </div>
-                                : <div></div>
-                            }
-                            {
-                                hasilAmbilData.length > 0 ? 
-                                    <div>
-                                        <select name="category" id="category" onChange={getClick} className=' block border-2 border-slate-400 w-full rounded p-2 bg-white' size={4}>
-                                            {
-                                                hasilAmbilData.map((hasil) => (
-                                                    <option key={hasil.id} value={hasil.id}>{hasil.nama_barang}</option>
-                                                ))
-                                            }
-                                        </select>
-                                    </div>
-                                :   null
-                            }
-                        </div>
-                    </div>
-                    
-                    <div className='mt-3 mx-4'>
-                        <div className='mx-5 mt-2 mb-4'>
-                            <label htmlFor="deskripsi" className=''>Deskripsi : </label>
-                            <textarea id='deskripsi' type="text" className='mt-2 block border-2 border-slate-400 w-full rounded p-2 placeholder-shown:italic' placeholder='Masukkan Deskripsi Barang' onChange={e => setDeskripsi(e.target.value)} value={deskripsi}/>
-                            <p className='text-sm mt-1'>Tulis deskripsi barang tanpa menggunakan emoticon.</p>
-                        </div>
-                    </div>
-                    <div className='mt-3 mx-4'>
-                        <div className='mx-5 mt-2 mb-4'>
-                            <label htmlFor="harga" className=''>Harga : </label>
-                            <input id='harga' disabled type="number" ref={hargaBarang} className='mt-2 block border-2 border-slate-400 w-full rounded p-2 bg-slate-300' />
-                        </div>
-                    </div>
-                    <div className='mt-3 mx-4'>
-                        <div className='mx-5 mt-2 mb-4'>
-                            <label htmlFor="banyak" className=''>Banyak : </label>
-
-                            <div className='flex'>
-                                {/* {
-                                    dataDipilih.length > 0 ? 
-                                    <div> */}
-                                        {
-                                            banyak === 1 ? 
-                                                <button type='button' className='w-10 h-10 text-black border-2 text-xl bg-slate-400' disabled>-</button> :
-                                                <button type='button' className='w-10 h-10 text-green-400 border-2 hover:bg-slate-400 duration-300 text-xl' onClick={minus}>-</button>
-                                        }                                
-                                        <p className='items-center flex px-7'>{banyak}</p>
-                                        {
-                                            banyak === maxPesan ? 
-                                                <button type='button' className='w-10 h-10 text-black border-2 text-xl bg-slate-400' disabled>+</button> :
-                                                <button type='button' className='w-10 h-10 text-green-400 border-2 hover:bg-slate-400 duration-300 text-xl' onClick={plus}>+</button>
-
-                                        }
-                                    {/* </div> : 
-                                    <div className='flex'>
-                                        <button className='w-10 h-10 text-black border-2 text-xl bg-slate-400' disabled>-</button>
-                                        <p className='items-center flex px-7'>{banyak}</p>
-                                        <button className='w-10 h-10 text-green-400 border-2 hover:bg-slate-400 duration-300 text-xl'>+</button>
-                                    </div>
-                                } */}
-
-
-
-                                
+                <div className='mt-3 mx-4'>
+                    <div className='mx-5 mt-2 mb-4'>
+                        <label htmlFor="nama" className=''>Nama Barang / Jasa : </label>
+                        <input id='nama_barang' type="text" className='mt-2 block border-2 border-slate-400 w-full rounded p-2 placeholder-shown:italic' placeholder='Masukkan Nama Barang' onChange={getBarang} ref={namaBarang}/>
+                        {
+                            dataDipilih.length > 0 ? 
+                            <div>
+                                <p className='text-sm mt-1' id='span_stock'>Stock barang sekarang : <b>{dataDipilih[0].stok_barang}</b></p>
                             </div>
-                            <p className='text-sm mt-1'>Masukkan banyak barang {`< / =`} stok barang yang dipilih 
-                            </p>
-                        </div>
+                            : <div></div>
+                        }
+                        {
+                            hasilAmbilData.length > 0 ? 
+                                <div>
+                                    <select name="category" id="category" onChange={getClick} className=' block border-2 border-slate-400 w-full rounded p-2 bg-white' size={4}>
+                                        {
+                                            hasilAmbilData.map((hasil) => (
+                                                <option key={hasil.id} value={hasil.id}>{hasil.nama_barang}</option>
+                                            ))
+                                        }
+                                    </select>
+                                </div>
+                            :   null
+                        }
                     </div>
-                    <div className='mt-3 mx-4'>
-                        <div className='mx-5 mt-2 mb-4'>
-                            <label htmlFor="total" className=''>Total : </label>
-                            <input id='total' disabled type="number" className='mt-2 block border-2 border-slate-400 w-full rounded p-2 bg-slate-300' value={total}/>
-                        </div>
+                </div>
+                
+                <div className='mt-3 mx-4'>
+                    <div className='mx-5 mt-2 mb-4'>
+                        <label htmlFor="deskripsi" className=''>Deskripsi : </label>
+                        <textarea id='deskripsi' type="text" className='mt-2 block border-2 border-slate-400 w-full rounded p-2 placeholder-shown:italic' placeholder='Masukkan Deskripsi Barang' onChange={e => setDeskripsi(e.target.value)} value={deskripsi}/>
+                        <p className='text-sm mt-1'>Tulis deskripsi barang tanpa menggunakan emoticon.</p>
                     </div>
-                    <div className="border-t border-slate-400 flex justify-end ">
-                        <div className='mt-4 align-bottom mr-5'>
-                            <button className='rounded px-3 py-2 border border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-black hover:duration-300' type='submit'>Tambah Barang</button>
-                            <button className='ml-4 rounded px-3 py-2 border border-black text-black hover:bg-slate-600 hover:text-white hover:duration-300' onClick={()=>closeModal(false)} >CLOSE</button>
-                        </div>
+                </div>
+                <div className='mt-3 mx-4'>
+                    <div className='mx-5 mt-2 mb-4'>
+                        <label htmlFor="harga" className=''>Harga : </label>
+                        <input id='harga' disabled type="number" ref={hargaBarang} className='mt-2 block border-2 border-slate-400 w-full rounded p-2 bg-slate-300' />
                     </div>
-                </form>
+                </div>
+                <div className='mt-3 mx-4'>
+                    <div className='mx-5 mt-2 mb-4'>
+                        <label htmlFor="banyak" className=''>Banyak : </label>
+
+                        <div className='flex'>
+                            {
+                                banyak === 1 ? 
+                                    <button type='button' className='w-10 h-10 text-black border-2 text-xl bg-slate-400' disabled>-</button> :
+                                    <button type='button' className='w-10 h-10 text-green-400 border-2 hover:bg-slate-400 duration-300 text-xl' onClick={minus}>-</button>
+                            }                                
+                            <p className='items-center flex px-7'>{banyak}</p>
+                            {
+                                banyak === maxPesan ? 
+                                    <button type='button' className='w-10 h-10 text-black border-2 text-xl bg-slate-400' disabled>+</button> :
+                                    <button type='button' className='w-10 h-10 text-green-400 border-2 hover:bg-slate-400 duration-300 text-xl' onClick={plus}>+</button>
+
+                            }
+                        </div>
+                        <p className='text-sm mt-1'>Masukkan banyak barang {`< / =`} stok barang yang dipilih 
+                        </p>
+                    </div>
+                </div>
+                <div className='mt-3 mx-4'>
+                    <div className='mx-5 mt-2 mb-4'>
+                        <label htmlFor="total" className=''>Total : </label>
+                        <input id='total' disabled type="number" className='mt-2 block border-2 border-slate-400 w-full rounded p-2 bg-slate-300' value={total}/>
+                    </div>
+                </div>
+                <div className="border-t border-slate-400 flex justify-end ">
+                    <div className='mt-4 align-bottom mr-5'>
+                        <button className='rounded px-3 py-2 border border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-black hover:duration-300' onClick={TambahBarang} >Tambah Barang</button>
+                        <button className='ml-4 rounded px-3 py-2 border border-black text-black hover:bg-slate-600 hover:text-white hover:duration-300' onClick={()=>closeModal(false)} >CLOSE</button>
+                    </div>
+                </div>
             </div>
         </div>
     )
