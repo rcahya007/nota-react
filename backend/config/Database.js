af237e5cbd9c6b0ff5e2a251ca2b33fcabe0ace8
RendyCahyaE <rendycahya6@gmail.com>
2022-08-21 17:26:36 +0700

    .

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,7 +1,7 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('nota_react','root','',{
-    host: "localhost",
+const db = new Sequelize('freedb_nota_react','freedb_blog-root','%s@4@68t89@qxKZ',{
+    host: "sql.freedb.tech",
     dialect: "mysql"
 });
 
