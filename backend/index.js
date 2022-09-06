f90042dfa6367c7e6de5a2e1aaf8ccc45b0663ba
Rendy Cahya <89686508+rendycahya@users.noreply.github.com>
2022-09-06 16:56:13 +0700

    .

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,32 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const cors = require("cors");
 const fileUpload = require("express-fileupload");
 // import db from "./config/Database.js";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.static("public"));
 
 const db = require("./models");
 
-db.sequelize.sync()
-    .then(()=>{
-        console.log("Synced db.");
-    })
-    .catch((err)=>{
-        console.log("Failed to sync db: " + err.message);
-    });
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
 
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to test Crud React Express First Time" });
+  res.json({ message: "Welcome to test Crud React Express First Time" });
 });
-
 
 require("./routes/barang.routes")(app);
 require("./routes/transaction.routes")(app);
 require("./routes/user.routes")(app);
-
 
 const port = process.env.PORT || 8080;
 
@@ -40,6 +39,6 @@ const port = process.env.PORT || 8080;
 //     console.error(error);
 // }
 
-app.listen(port, ()=> {
-    console.log('Server running at port 8080')
+app.listen(port, () => {
+  console.log("Server running at port 8080");
 });
--- backend/models/DetailTransaction.js
+++ backend/models/DetailTransaction.js
@@ -1,40 +1,42 @@
-module.exports = (sequelize,Sequelize) => {
-    const DetailTransaction = sequelize.define('detail_transactions',{
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true,
-            autoIncrement: true,
+module.exports = (sequelize, Sequelize) => {
+  const DetailTransaction = sequelize.define(
+    "detail_transactions",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      id_transaction: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "transactions",
+          key: "id",
         },
-        id_transaction: {
-            type: Sequelize.INTEGER,
-            references:{
-                model: 'transactions',
-                key: 'id'
-            },
+      },
+      id_barang: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "barang",
+          key: "id",
         },
-        id_barang: {
-            type: Sequelize.INTEGER,
-            references:{
-                model: 'barang',
-                key: 'id'
-            },
-        },
-        deskripsi_pembelian: {
-            type: Sequelize.STRING,
-            allowNull: true,
-        },
-        banyak_barang: {
-            type: Sequelize.STRING,
-            allowNull: true,
-        },
-        total_harga_barang: {
-            type: Sequelize.FLOAT,
-            allowNull: true,
-        },
-    },{
-        freezeTableName: true,
-    }); 
-    return DetailTransaction;
-}
-
-
+      },
+      deskripsi_pembelian: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      banyak_barang: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      total_harga_barang: {
+        type: Sequelize.FLOAT,
+        allowNull: true,
+      },
+    },
+    {
+      freezeTableName: true,
+    }
+  );
+  return DetailTransaction;
+};
